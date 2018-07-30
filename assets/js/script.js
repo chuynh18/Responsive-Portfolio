f2f71ef86ba24fd6fd885c379c2e3a8e4778ac06
5820K <38088640+chuynh18@users.noreply.github.com>
2018-07-30 16:27:21 -0700

    animation test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,32 @@
 "use strict";
 
 window.onload = function() {
-    anime({
-        targets: 'div',
-        translateX: [
-          { value: 100, duration: 1200 },
-          { value: 0, duration: 800 }
-        ],
-        rotate: '1turn',
-        backgroundColor: '#FFF',
-        duration: 2000,
-        loop: true
-    });
+
+    anime.timeline()
+        .add({
+            targets: '#welcome',
+            translateY: ["45%", "50%"],
+            opacity: 100,
+            duration: 1200,
+            easing: "linear"
+        })
+        .add({
+            targets: '#welcome',
+            translateY: ["50%", "55%"],
+            opacity: 0,
+            duration: 1200,
+            easing: "linear"
+        })
+        .add({
+            targets: '.blockall',
+            opacity: 0,
+            duration: 1500,
+            easing: "linear"
+        })
+        .add({
+            targets: '.content',
+            translateY: ["40%", "0%"],
+            duration: 1700,
+            offset: '-=1500',
+        });
 }
